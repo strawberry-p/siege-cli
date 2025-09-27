36f160ad3ff8cdc19cb18d11e29706aa34704dee
strawberry-p <strawberry@example.com>
2025-09-27 09:49:12 +0200

    gitignore test WE, yes WE will be committing our SESSION KEYS to the repo

--- siege.py
+++ siege.py
@@ -5,6 +5,18 @@ SESSION_FILE = 'session.json'
 with open(SESSION_FILE) as file:
     sessionData = json.load(file)
 session = r.Session()
-def page_soup(url):
-    res = session.get(url)
+lastHeader = {}
+#note: siege does some funky stuff with cookies
+#apparently, it gets rotated every request, with the server response's
+#Set-Cookie header being saved as HttpOnly for the next request
+#i will probably need to obtain my own cookie at once
+def page_soup(url,cf_clearance,cookie=None):
+    global lastHeader
+    if cookie:
+        res = session.get(url,headers={"cookie":f"_siege_session={cookie}"})
+    else:
+        res = session.get(url)
+    print(res.status_code)
+    lastHeader = res.headers
+    print(lastHeader)
     return BeautifulSoup(res.content,"html.parser")
